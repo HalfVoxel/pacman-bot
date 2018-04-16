8c2b6c0ac6f90f8174753567267ce4aeb886f467
Aron Granberg <aron.granberg@gmail.com>
2018-04-16 11:41:48 +0200

    PEP8 automatic code formatting

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -13,7 +13,9 @@
 
 
 from captureAgents import CaptureAgent
-import random, time, util
+import random
+import time
+import util
 from game import Directions
 import game
 
@@ -21,8 +23,9 @@ import game
 # Team creation #
 #################
 
+
 def createTeam(firstIndex, secondIndex, isRed,
-               first = 'DummyAgent', second = 'DummyAgent'):
+               first='DummyAgent', second='DummyAgent'):
   """
   This function should return a list of two agents that will form the
   team, initialized using firstIndex and secondIndex as their agent
@@ -44,6 +47,7 @@ def createTeam(firstIndex, secondIndex, isRed,
 ##########
 # Agents #
 ##########
+
 
 class DummyAgent(CaptureAgent):
   """
@@ -77,7 +81,6 @@ class DummyAgent(CaptureAgent):
     Your initialization code goes here, if you need any.
     '''
 
-
   def chooseAction(self, gameState):
     """
     Picks among actions randomly.
@@ -89,4 +92,3 @@ class DummyAgent(CaptureAgent):
     '''
 
     return random.choice(actions)
-
