e926d832359ae02628dc67e9bae8af2c2b69b37f
Aron Granberg <aron.granberg@gmail.com>
2018-04-16 13:26:27 +0200

    Add position estimator

--- bot.py
+++ bot.py
@@ -18,6 +18,8 @@ import time
 import util
 from game import Directions
 import game
+import numpy as np
+import math
 
 #################
 # Team creation #
@@ -48,8 +50,76 @@ def createTeam(firstIndex, secondIndex, isRed,
 # Agents #
 ##########
 
+SIGHT_RANGE = 5
+
+class PositionEstimator:
+  ''' Estimates the position of agents '''
+  def __init__(self, agent_index, gameState):
+    self.index = agent_index
+    self.walls = gameState.getWalls()
+    self.width = self.walls.width
+    self.height = self.walls.height
+    self.map = np.zeros((self.width, self.height))
+    initial_position = gameState.getInitialAgentPosition(self.index)
+    self.map[initial_position[0],initial_position[1]] = 1.0
+    self.dx = [+1, 0, -1, 0, 0]
+    self.dy = [0, +1, 0, -1, 0]
+
+  def update(self, bot, gameState):
+    noisy_distance = gameState.getAgentDistances()[self.index]
+    new_map = np.zeros((self.width, self.height))
+
+    # If the target is directly observable, then just set that tile to have a probability of 1.0
+    target_pos = gameState.getAgentPosition(self.index)
+    if target_pos is not None:
+      new_map[target_pos[0], target_pos[1]] = 1.0
+      self.map = new_map
+      return
+
+    # Calculate probability of being in each tile given that the agent moves randomly
+    for x in range(0, self.width):
+      for y in range(0, self.height):
+        if not self.walls[x][y]:
+          numOptions = 0
+          for i in range(len(self.dx)):
+            if not self.walls[x+self.dx[i]][y+self.dy[i]]:
+              numOptions += 1
+
+          value = self.map[x,y] / numOptions
+          for i in range(len(self.dx)):
+            if not self.walls[x+self.dx[i]][y+self.dy[i]]:
+              new_map[x+self.dx[i], y+self.dy[i]] += value
+
+    pos = gameState.getAgentPosition(bot.index)
+    for x in range(0, self.width):
+      for y in range(0, self.height):
+        dist = abs(pos[0]-x) + abs(pos[1]-y)
+        if dist <= 5:
+          # We did not observe the agent, so it cannot be on this square because then we would have observed it
+          new_map[x,y] = 0
+        else:
+          new_map[x,y] *= gameState.getDistanceProb(dist, noisy_distance)
+
+    # Normalize
+    new_map /= np.sum(new_map)
+    self.map = new_map
+
+  def most_probable_position(self):
+    return np.unravel_index(self.map.argmax(), self.map.shape)
+
+  def draw_probabilities(self, bot, color):
+    for x in range(self.width):
+      for y in range(self.height):
+        f = math.pow(self.map[x][y], 0.01)
+        if f > 0:
+          bot.debugDraw([(x, y)], color * f)
+
 
 class DummyAgent(CaptureAgent):
+  def __init__(self, isRed):
+    super().__init__(isRed)
+    self.position_estimators = []
+
   """
   A Dummy agent to serve as an example of the necessary agent structure.
   You should look at baselineTeam.py for more details about how to
@@ -77,9 +147,8 @@ class DummyAgent(CaptureAgent):
     '''
     CaptureAgent.registerInitialState(self, gameState)
 
-    '''
-    Your initialization code goes here, if you need any.
-    '''
+    for i in range(0,4):
+      self.position_estimators.append(PositionEstimator(i, gameState))
 
   def chooseAction(self, gameState):
     """
@@ -90,5 +159,12 @@ class DummyAgent(CaptureAgent):
     '''
     You should change this in your own agent.
     '''
+    self.debugClear()
+    colors = np.array([[1,0,0], [0,1,0], [0,0,1], [1,1,0]])
+
+    for pi in range(len(self.position_estimators)):
+      p = self.position_estimators[pi]
+      p.update(self, gameState)
+      p.draw_probabilities(self, colors[pi])
 
     return random.choice(actions)
